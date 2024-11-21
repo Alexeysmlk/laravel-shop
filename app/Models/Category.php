a9d38e75836b65e601ed1e8421d669d0241ecf98
Самойлик Алексей <samoilikaleksey@yandex.ru>
2024-11-21 19:39:55 +0300

    add functional for database

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,30 +2,22 @@
 
 namespace App\Models;
 
+use App\Traits\Models\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
+    use HasSlug;
 
     protected $fillable = [
         'title',
     ];
 
-    protected static function boot(): void
+    public function products(): BelongsToMany
     {
-        parent::boot();
-
-        // TODO update
-        static::creating(function (Category $category) {
-            $category->slug = $category->slug ?? str($category->title)->slug();
-        });
-    }
-
-    public function products(): BelongsTo
-    {
-        return $this->belongsTo(Product::class);
+        return $this->belongsToMany(Product::class);
     }
 }
